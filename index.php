0096349ed21fa8f1258ddf25eebbbabf3a01f7f8
Caswell Nziyane <118727469+221509313@users.noreply.github.com>
2024-07-20 19:50:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>KhaniNkuna</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-1" id="Navbar">
+        <div class="container-fluid mt-5">
+            <a class="navbar-brand" href="#">NkunaAdvisory.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#About">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Services">Services</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Contacts">Contacts</a>
+                    </li>
+                    <li class="nav-item dropdown">
+
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="home-content" id="home">
+
+        <h2><b>KhaniNkuna Legal Services</b></h2>
+        <div class="my-card">
+            <div class="card me-3 bg-secondary pb-5" style="width: 18rem; ">
+
+                <div class="card-body">
+                    <h5 class="card-title">Navigate Legal Matters</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere<i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="card me-3 bg-secondary pb-4" style="width: 18rem;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Marriage and Lobola Guidance</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="card me-3 bg-secondary pb-5" style="width: 18rem;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Legal Advisory Services</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="About bg-dark text-light " id="About">
+        <div class="style pb-5 " style="background-color:black;">
+            <div class="container ">
+                <div class="row pt-3">
+                    <div class="col-2 md-12">
+                        <h2>Our Story</h2>
+                    </div>
+                    <div class="col-2">
+                        <div class="vertical-line"></div>
+                    </div>
+                    <div class="col-8 md-12">
+                        <h2>KhaniNkuna Legal Services</h2>
+                        <p>
+                            KhaniNkuna offers more than just legal assistance; we provide a platform for discussing marriages, lobola traditions, and offer guidance on legal issues. Our commitment to exceptional service, personalized consultations, and a supportive environment aims to empower you to make informed decisions and achieve your legal goals. Whether you are well-versed in legal matters or seeking guidance for the first time, we welcome you to join us on this journey towards a legally secure future.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="Services mt-5 ">
+                <p>Tailored legal solutions to elevate your legal experience</p>
+                <div class="buttons">
+                    <button class="btn btn-lg btn-primary">Contact</button>
+                    <button class="btn btn-lg btn-light">Reviews</button>
+                </div>
+            </div>
+        </div>
+
+        <!-----cards--->
+        <h2 style="text-align: center; margin-top:16px;">Our Services</h2>
+        <div class="my-card" style="color: rgba(0, 0, 0, 0.5);" id="Services">
+
+            <div class="card me-3 bg-secondary pb-5" style="width: 18rem; ">
+
+                <div class="card-body">
+                    <h5 class="card-title">Legal consultations</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere<i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="card me-3 bg-secondary pb-5" style="width: 18rem;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Contract Drafting</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="card me-3 bg-secondary pb-5" style="width: 18rem;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Online consultations</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary bg-transparent">Go somewhere <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <!----text Coursel--->
+        <div id="textCarousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <h3>Your Giuide to legal Clarity and Peace of Mind<i class="fas fa-star"></i></h3>
+                </div>
+                <div class="carousel-item">
+                    <h3>Your Giuide to legal Clarity and Peace of Mind<i class="fas fa-star"></i></h3>
+                </div>
+                <div class="carousel-item">
+                    <h3>Your Giuide to legal Clarity and Peace of Mind<i class="fas fa-star"></i></h3>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#textCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#textCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <div class="contact-form" id="Contacts">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 mb-4 mt-5">
+                        <img src="images/Nkuna-Legal.jpg" alt="Legal-image">
+                        <div>
+                            <a href="#Navbar" style="text-decoration: none; text-align: center; color: green; height: 100%; width: 100%;">
+                                <i class="fas fa-arrow-up fa-3x"></i> <!-- You can use fa-2x, fa-3x, fa-4x, fa-5x, fa-6x for different sizes -->
+
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <h2>Get in touch with us</h2>
+                        <p>For more information or legal assistance, feel free to contact us.</p>
+                        <div class="contact-details">
+                            <span class="email">NkunaLegal@Gmail.com</span>
+                            <span class="number">+27 65 970 3071</span>
+                        </div>
+
+                        <form action="https://api.web3forms.com/submit" method="POST">
+
+                            <input type="hidden" name="access_key" value="3315f2dc-08a9-4f8c-af3e-af7b6d654798">
+                            <label for="Name">Name</label><br>
+                            <input type="text" name="Name" placeholder="Your name here" required>
+                            <br>
+                            <label for="Email">Email</label><br>
+                            <input type="email" name="Email" placeholder="Your email here" required>
+                            <br>
+                            <label for="Message">Message</label><br>
+                            <textarea name="Message" id="Message" cols="30" rows="10" placeholder="Your message here" required></textarea>
+                            <br>
+                            <button class="btn btn-lg btn-primary" type="submit">Submit</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!----javascript----->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
